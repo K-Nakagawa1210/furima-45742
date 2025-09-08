9d3a13dcf5027079ee67933a7729f8a43c92b9e6
K-Nakagawa1210 <n349517k@gmail.com>
2025-09-08 11:39:31 +0900

    Fix: ユーザーモデルとdevise設定のコンフリクト解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
