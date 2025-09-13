26ba37bcf94dcc894f3bb2f41d2a925c42c8a68a
K-Nakagawa1210 <n349517k@gmail.com>
2025-09-13 14:52:22 +0900

    itemモデルのバリテーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,24 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  has_one_attached :image
+
+  belongs_to :user
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :condition_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :shipping_fee_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :shipping_day_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :price,
+              numericality: { only_integer: true,
+                              greater_than_or_equal_to: 300,
+                              less_than_or_equal_to: 9_999_999,
+                              message: "は半角数字で300円〜9,999,999円の範囲で入力してください" }
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,14 +3,14 @@ FactoryBot.define do
     name { "テスト商品" }
     description { "説明文" }
     price { 1000  }
-    category_id { 1 }
-    condition_id { 1 }
-    shipping_fee_id { 1 }
-    prefecture_id { 1 }
-    shipping_day_id { 1 }
+    category_id { 2 }
+    condition_id { 2 }
+    shipping_fee_id { 2 }
+    prefecture_id { 2 }
+    shipping_day_id { 2 }
     association :user
     after(:build) do |item|
-      item.image.attach(io: File.open(Rails.root.join('spec/fixtures/test_image.png')), filename: 'test_image.png', content_type: 'image/png')
+      item.image.attach(io: File.open(Rails.root.join('spec/fixtures/files/test_image.png')), filename: 'test_image.png', content_type: 'image/png')
     end
   end
 end
