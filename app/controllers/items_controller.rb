500a66dffe789fd56ee71abfa6a0251c95f2b517
K-Nakagawa1210 <n349517k@gmail.com>
2025-09-18 02:32:50 +0900

    ルーティングとコントローラの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -8,9 +7,6 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-  end
-
-  def show
   end
 
   def create
@@ -22,13 +18,8 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
@@ -39,10 +30,6 @@ class ItemsController < ApplicationController
       :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id,
       :image
     )
-  end  
-
-  def set_item
-    @item = Item.find(params[:id])
   end
 
 end
