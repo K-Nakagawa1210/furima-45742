dadfed765362fb90e81dec353e3f7c599c557a2d
K-Nakagawa1210 <n349517k@gmail.com>
2025-09-19 20:03:41 +0900

    デプロイの設定

--- db/migrate/20250905073114_add_columns_to_users.rb
+++ db/migrate/20250905073114_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :nickname, :string
+    add_column :users, :nickname, :string, :string unless column_exists?(:users, :nickname)
     add_column :users, :last_name, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name_kana, :string
