fea79212e7bb1220506cb0a6e5c5d547c9611efd
K-Nakagawa1210 <n349517k@gmail.com>
2025-09-23 19:07:21 +0900

    エラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
   before_action :move_to_root, only: [:index]
-  
+
   def index
     @order_address = OrderAddress.new
   end
@@ -14,14 +14,14 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render :index
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id, token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def pay_item
