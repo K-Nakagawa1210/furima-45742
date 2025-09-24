30f0a4f4b77813dbe10e08f17dd83ffa2ab4b02d
K-Nakagawa1210 <n349517k@gmail.com>
2025-09-24 22:50:05 +0900

    クレジットカード決済機能

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 4.0.0' 
   gem 'factory_bot_rails'
   gem 'faker'
+  gem 'dotenv-rails'
 end
 
 group :development do
@@ -82,3 +83,4 @@ gem 'image_processing', '~> 1.2'
 gem 'active_hash'
 gem 'pry-rails'
 gem 'payjp'
+
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,7 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
+      Rails.logger.debug(@order_address.errors.full_messages) 
       render :index, status: :unprocessable_entity
     end
   end
@@ -21,11 +22,12 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token).merge(user_id: current_user.id, item_id: @item.id)
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    item = Item.find(params[:item_id])
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
