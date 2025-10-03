8d028fc2130ef60b38060ad87693e814d413e74f
K-Nakagawa1210 <n349517k@gmail.com>
2025-10-03 10:10:49 +0900

    クレジットカード支払いの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,18 +4,20 @@ class OrdersController < ApplicationController
   before_action :move_to_root, only: [:index]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
   def create
+#    binding.pry
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
       @order_address.save
-      redirect_to root_path
+      return redirect_to root_path
     else
-      Rails.logger.debug(@order_address.errors.full_messages) 
-      render :index, status: :unprocessable_entity
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render 'index', status: :unprocessable_entity
     end
   end
 
@@ -27,7 +29,6 @@ class OrdersController < ApplicationController
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    item = Item.find(params[:item_id])
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
